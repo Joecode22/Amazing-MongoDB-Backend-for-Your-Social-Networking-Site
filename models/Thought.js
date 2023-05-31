5dab855f496ff17ba253ac4c31ffde87bd2c752d
joecode22 <joe.code.mckinney@gmail.com>
2023-05-30 22:34:37 -0500

    corrected the thought model so that the reaction model was not stand-alone but reaction field's subdocument schema in the thought model

--- models/Thought.js
+++ models/Thought.js
@@ -1,51 +1,68 @@
-//Thought Model/Schema
-const { Schema, model, Types } = require('mongoose');
+const { Schema, model } = require('mongoose');
+const moment = require('moment');
 
-//require the dateFormat module
-const dateFormat = require('../utils/dateFormat');
-
-const ThoughtSchema = new Schema(
-    {
-        thoughtText: {
-            type: String,
-            required: true,
-            //must be between 1 and 280 characters
-            minlength: 1,
-            maxlength: 280
-        },
-        createdAt: {
-            type: Date,
-            default: Date.now,
-            //use a getter method to format the timestamp on query
-            get: createdAtVal => dateFormat(createdAtVal)
-        },
-        username: {
-            type: String,
-            required: true
-        },
-        //use ReplySchema to validate data for a reply
-        replies: [ReactionSchema]
+const ReactionSchema = new Schema(
+  {
+    reactionId: {
+      type: Schema.Types.ObjectId,
+      default: () => new Types.ObjectId(),
     },
-    {
-        toJSON: {
-            virtuals: true,
-            getters: true
-        },
-        //prevents virtuals from creating duplicate of _id as `id`
-        id: false
+    reactionBody: {
+      type: String,
+      required: true,
+      maxLength: 280,
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      //use a getter method to format the timestamp on query
+      get: createdAtVal => moment(createdAtVal).format('MMM DD, YYYY [at] hh:mm a')
     }
+  },
+  {
+    toJSON: {
+      getters: true
+    },
+    id: false
+  }
 );
 
-// Create a virtual called reactionCount that retrieves the length of the thought's reactions array field on query.
+const ThoughtSchema = new Schema(
+  {
+    thoughtText: {
+      type: String,
+      required: true,
+      minLength: 1,
+      maxLength: 280,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: createdAtVal => moment(createdAtVal).format('MMM DD, YYYY [at] hh:mm a')
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    reactions: [ReactionSchema]
+  },
+  {
+    toJSON: {
+      virtuals: true,
+      getters: true
+    },
+    id: false
+  }
+);
+
 ThoughtSchema.virtual('reactionCount').get(function() {
-    return this.reactions.length;
+  return this.reactions.length;
 });
 
-//create the Thought model using the ThoughtSchema
 const Thought = model('Thought', ThoughtSchema);
 
-//export the Thought model
 module.exports = Thought;
-
-
-
