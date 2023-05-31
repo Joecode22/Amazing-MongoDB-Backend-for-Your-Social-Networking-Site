1de7e402a86bd395625438200e273ccc214cb7b2
joecode22 <joe.code.mckinney@gmail.com>
2023-05-30 22:00:21 -0500

    created the Reaction model

--- models/Reaction.js
+++ models/Reaction.js
@@ -4,4 +4,39 @@ const { Schema, model, Types } = require('mongoose');
 const dateFormat = require('../utils/dateFormat');
 const ReactionSchema = new Schema(
     {
-        
+        //reactionId uses mongoose's ObjectId data type and default value is set to a new ObjectId
+        reactionId: {
+            type: Schema.Types.ObjectId,
+            default: () => new Types.ObjectId()
+        },
+        //reactionBody is a string and is required with a max length of 280 characters
+        reactionBody: {
+            type: String,
+            required: true,
+            maxlength: 280
+        },
+        //username is a string and is required
+        username: {
+            type: String,
+            required: true
+        },
+        //createdAt uses the Date type and default value is set to the current timestamp
+        //use a getter method to format the timestamp on query
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: createdAtVal => dateFormat(createdAtVal)
+        }
+    },
+    {
+        toJSON: {
+            getters: true
+        }
+    }
+);
+
+//create the Reaction model using the ReactionSchema
+const Reaction = model('Reaction', ReactionSchema);
+
+//export the ReactionSchema
+module.exports = Reaction;
--- models/Thought.js
+++ models/Thought.js
@@ -39,6 +39,13 @@ const ThoughtSchema = new Schema(
 // Create a virtual called reactionCount that retrieves the length of the thought's reactions array field on query.
 ThoughtSchema.virtual('reactionCount').get(function() {
     return this.reactions.length;
-}
-);
+});
+
+//create the Thought model using the ThoughtSchema
+const Thought = model('Thought', ThoughtSchema);
+
+//export the Thought model
+module.exports = Thought;
+
+
 
